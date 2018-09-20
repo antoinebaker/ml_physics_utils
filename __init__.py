18d3102a016565f2235dd159dc5e26d0c410e0e1
Antoine Baker <antoine.baker59@gmail.com>
2018-09-20 13:42:55 +0200

    select database

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
-from .connection import get_table, TableResultHandler, DATABASE
-from .notebook import progressbar
-from .plot import plot_function, vega_plot
+from .connection import get_table, get_database, TableResultHandler
 from .experiment import teacher_student_scenario, run_multiple_experiments
-from .experiment import TableTask, Task
+from .experiment import TableTask, Task, DataFrameResultHandler
 from .scripting import task_runner
--- connection.py
+++ connection.py
@@ -4,28 +4,33 @@ import logging
 import os
 import dataset
 
-def get_database():
+def get_default_database_url():
     try:
-        DATABASE_URL = os.environ["DATABASE_URL"]
+        return os.environ["DATABASE_URL"]
     except KeyError:
         home = os.environ["HOME"]
-        DATABASE_URL = f"sqlite:///{home}/ml_physics.db"
-    return dataset.connect(DATABASE_URL)
+        return f"sqlite:///{home}/ml_physics.db"
 
-DATABASE = get_database()
+DEFAULT_DATABASE_URL = get_default_database_url()
 
-def get_table(table_name):
-    if table_name not in DATABASE.tables:
+def get_database(database_url = None):
+    database_url = database_url or DEFAULT_DATABASE_URL
+    return dataset.connect(database_url)
+
+def get_table(table_name, database_url = None):
+    database = get_database(database_url)
+    if table_name not in database.tables:
         logging.error(f"{table_name} does not exist")
         return None
-    table = DATABASE[table_name]
+    table = database[table_name]
     records = [record for record in table.all()]
     return pd.DataFrame(records)
 
 
 class TableResultHandler():
-    def __init__(self, table_name, overwrite=False):
-        table = DATABASE[table_name]
+    def __init__(self, table_name, overwrite=False, database_url = None):
+        database = get_database(database_url)
+        table = database[table_name]
         if overwrite:
             logging.warning(f"Dropping table {table_name}")
             table.drop()

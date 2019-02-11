47c54864f8e807923eb15bf51d66339e58deaeb5
Antoine Baker <antoine.baker59@gmail.com>
2019-02-11 11:48:17 +0100

    improve qplot

--- plot.py
+++ plot.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def plot_function(f, xlim=(-3,3)):
+def plot_function(f, xlim=(-3, 3)):
     x = np.linspace(xlim[0], xlim[1], 100)
     plt.plot(x, f(x))
     plt.title(f"{f.__name__}")
@@ -32,13 +32,17 @@ def as_query(query_format, **kwargs):
     return query
 
 
+LINESTYLES = ['-', '--', '-.', ':']
+MARKERS = ['.', 'x', 'o', 'v', '^', '<', '>', 's', 'D']
+
+
 def get_plot_instructions(data, aes_fields):
     def get_values(field):
         return sorted(data[field].unique())
     aes_palette = dict(
         color=sns.color_palette(n_colors=10),
-        linestyle=['-', '--', '-.', ':'],
-        marker=['.', 'x', 'o', 'v', '^', '<', '>', 's', 'D']
+        linestyle=LINESTYLES,
+        marker=MARKERS
     )
     field_choices = {
         field: get_values(field)
@@ -68,81 +72,125 @@ def get_plot_instructions(data, aes_fields):
                     palette = aes_palette[aes]
                     options[aes] = palette[idx_value]
         instructions.append(dict(
-            query=query, row=pos['row'], column=pos['column'],
+            query=query, row=pos["row"], column=pos["column"],
             options=options, title=title, label=label
         ))
     return instructions
 
 
+def replace(label, mapping):
+    "Replace key-> val in string `label`, for all key->val in `mapping`"
+    if mapping:
+        for old, new in mapping.items():
+            label = label.replace(old, new)
+    return label
+
 def qplot(data, x, y,
           color=None, column=None, row=None, marker=None, linestyle=None,
           xlog=False, ylog=False, xlim=None, ylim=None,
-          y_markers="-", sharex=True, sharey=True, figsize=4,
+          y_markers=None, sharex=True, sharey=True, figsize=4,
           y_legend=False,
           rename=None, font_size=12, usetex=False
           ):
+    # check args
+    y_multiple = isinstance(y, list)
+    if y_multiple:
+        if not isinstance(y_markers, list) or len(y) != len(y_markers):
+            raise ValueError("y_markers must be a list of same length as y")
+        if marker is not None:
+            raise ValueError("cannot use marker in y_multiple mode")
+        if linestyle is not None:
+            raise ValueError("cannot use linestyle in y_multiple mode")
     plt.rc('text', usetex=usetex)
     plt.rc('font', family='serif', size=font_size)
+    # get plotting instructions
     aes_fields = dict(
         color=color, column=column, row=row, marker=marker, linestyle=linestyle
     )
     instructions = get_plot_instructions(data, aes_fields)
+    # set figure
     nrows = max(instruction["row"] for instruction in instructions) + 1
     ncols = max(instruction["column"] for instruction in instructions) + 1
     if isinstance(figsize, float) or isinstance(figsize, int):
         figsize = (figsize * ncols, figsize * nrows)
-
-    def replace(label, mapping):
-        if mapping:
-            for old, new in mapping.items():
-                label = label.replace(old, new)
-        return label
     fig, axs = plt.subplots(
         nrows, ncols, squeeze=False, figsize=figsize,
         sharex=sharex, sharey=sharey
     )
+    # set scales
     if xlog:
         plt.xscale("log")
     if ylog:
         plt.yscale("log")
-    if not isinstance(y, list):
-        y = [y]
-    if not isinstance(y_markers, list):
-        y_markers = [y_markers]
-    if len(y)!=len(y_markers):
-        raise ValueError("y and y_markers must have the same length")
+    # iterate over plotting instructions
     for instruction in instructions:
+        # get data
         if instruction["query"]:
             df = data.query(instruction["query"])
         else:
             df = data
-        ax = axs[instruction['row'], instruction['column']]
-        title = replace(instruction['title'], rename)
-        for i, (y_var, y_marker) in enumerate(zip(y, y_markers)):
-            if y_legend:
-                label = instruction['label'] + " " + y_var
-            else:
-                label = instruction['label'] if i ==0 else ""
-            label = replace(label, rename)
-            if y_marker=="-":
-                ax.plot(df[x], df[y_var], **instruction['options'], label=label)
-            else:
-                ax.scatter(
-                    df[x], df[y_var], marker=y_marker,
-                    **instruction['options'], label=label
+        # get ax
+        ax = axs[instruction["row"], instruction["column"]]
+        # plot ax
+        if y_multiple:
+            print("y_multipl mode")
+            # iterate over y
+            for i, (y_var, y_marker) in enumerate(zip(y, y_markers)):
+                # set label
+                if y_legend:
+                    label = instruction["label"] + " " + y_var
+                else:
+                    label = instruction["label"] if i == 0 else ""
+                label = replace(label, rename)
+                # set aes
+                if y_marker in LINESTYLES:
+                    instruction["options"].update(linestyle=y_marker, marker="")
+                elif y_marker in MARKERS:
+                    instruction["options"].update(linestyle="", marker=y_marker)
+                else:
+                    raise ValueError(f"unkown marker {y_marker}")
+                # plot
+                ax.plot(
+                    df[x], df[y_var], **instruction["options"], label=label
                 )
+        else:
+            print("not y_multiple mode")
+            # set label
+            if y_legend:
+                label = instruction["label"] + " " + y
+            else:
+                label = instruction["label"]
+            label = replace(label, rename)
+            # set aes
+            if not linestyle and not marker:
+                instruction["options"].update(linestyle="-", marker="")
+            if linestyle and not marker:
+                instruction["options"].update(marker="")
+            if not linestyle and marker:
+                instruction["options"].update(linestyle="")
+            # plot
+            ax.plot(
+                df[x], df[y], **instruction['options'], label=label
+            )
+        # set title
+        title = replace(instruction['title'], rename)
         ax.set(title=title)
+    # set limits
     if xlim:
         plt.xlim(xlim)
     if ylim:
         plt.ylim(ylim)
-    ylabel= "" if y_legend else ", ".join(y)
+    # set xlabel and ylabel
+    if y_legend:
+        ylabel = ""
+    else:
+        ylabel = ", ".join(y) if y_multiple else y
     xlabel = replace(x, rename)
     ylabel = replace(ylabel, rename)
     any_label = any(instruction['label'] for instruction in instructions)
     for ax in axs.ravel():
         ax.set(xlabel=xlabel, ylabel=ylabel)
-        if y_legend or any_label:
+        if any_label or y_legend:
             ax.legend()
     fig.tight_layout()
 
